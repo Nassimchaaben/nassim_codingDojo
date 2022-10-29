3512da8c5db6db437f6cbb85afa5498be5d40f15
Nassim chaaben <nassimchaaben3@gmail.com>
2022-10-29 15:47:38 +0100

    update

--- Python-Flask/Week1/Day5/practice-assignment/checkerboard/server.py
+++ Python-Flask/Week1/Day5/practice-assignment/checkerboard/server.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template("index.html")	# notice the 2 new named arguments!
+@app.route('/') 
+def check():
+    return  render_template('index.html',x=4,y=4,color1="green",color2="beige")
+
+@app.route('/<int:x1>') 
+def check2(x1):
+    x2= x1//2
+
+    return  render_template('4.html',x2=x2,color1="green",color2="beige")
+
+@app.route('/<int:x1>/<int:y1>') 
+def check3(x1,y1):
+    x2= x1//2
+    y2= y1//2
+
+    return  render_template('xy.html',x2=x2,y2=y2,color1="green",color2="beige")
+
+@app.route('/<int:x1>/<int:y1>/<string:color1>/<string:color2>') 
+def check4(x1,y1,color1,color2):
+    x2= x1//2
+    y2= y1//2
+    return  render_template('xy.html',x2=x2,y2=y2,color1=color1,color2=color2)
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
