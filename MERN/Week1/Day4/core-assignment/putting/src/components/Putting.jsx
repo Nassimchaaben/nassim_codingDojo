0f6ea106b7bc99196de46cce66bb234f7550706c
Nassim chaaben <nassimchaaben3@gmail.com>
2022-11-24 18:29:50 +0000

    first-commit

--- MERN/Week1/Day4/core-assignment/putting/src/components/Putting.jsx
+++ MERN/Week1/Day4/core-assignment/putting/src/components/Putting.jsx
@@ -7,26 +7,23 @@ export default class Putting extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
             age:this.props.age // (6)
         }
     }
-hi() {
-        
+hi() { 
         this.setState({
-             age:this.state.age + 1
-            },() => console.log(this.state.age))
+        age:this.state.age + 1
+        },() => console.log(this.state.age))
     }
         
-  render() {
-                
-            return(
-                <div >
-                  <h1>{this.props.firstname}, {this.props.lastname}</h1>
-                  <p>age: {this.state.age}</p>
-                  <p>Hair Color: {this.props.HairColor}</p>
-                  <button onClick={() => { this.hi() }}>Birthday Button for {this.props.firstname}, {this.props.lastname}</button><br />
-                </div>
+  render() {    
+        return(
+            <div >
+                <h1>{this.props.firstname}, {this.props.lastname}</h1>
+                <p>age: {this.state.age}</p>
+                <p>Hair Color: {this.props.HairColor}</p>
+                <button onClick={() => { this.hi() }}>Birthday Button for {this.props.firstname}, {this.props.lastname}</button><br />
+             </div>
                 )
   }
 }
