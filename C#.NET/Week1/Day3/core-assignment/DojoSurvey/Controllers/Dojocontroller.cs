700e3631fbafc3b28a4a16302147b7dc3f13c438
Nassim chaaben <nassimchaaben3@gmail.com>
2022-12-22 21:50:14 +0000

    commit

---
+++ C#.NET/Week1/Day3/core-assignment/DojoSurvey/Controllers/Dojocontroller.cs
@@ -0,0 +1,34 @@
+namespace DOJOSURVEY.Controllers;
+using Microsoft.AspNetCore.Mvc;
+public class DojoContoller : Controller
+{
+    [HttpGet]
+    [Route("")]
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpGet]
+    [Route("Second/user")]
+    public ViewResult Second()
+    {
+        return View("SecondView");
+    }
+ 
+
+    [HttpPost]
+    [Route("process")]
+    public IActionResult Process(string Name, string location, string language, string comment)
+    {
+        if (Name == null){
+            return View("Index");
+        }
+        ViewBag.Name = Name;
+        ViewBag.location  =location;
+        ViewBag.language = language;
+        ViewBag.comment = comment;
+        
+        return RedirectToAction("Second");
+    }
+}
