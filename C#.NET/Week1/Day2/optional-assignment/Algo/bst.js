3a1539a78f77c84453ffc7fbdb27ad4cc49a8b06
Nassim chaaben <nassimchaaben3@gmail.com>
2022-12-26 09:49:21 +0000

    commit

---
+++ C#.NET/Week1/Day2/optional-assignment/Algo/bst.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor (value){
+        this.value = value 
+        this.right = null 
+        this.left = null
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null 
+    }
+    isEmpty (){
+        
+            return this.root===null   
+        }
+    min (){
+        let runner =this.root;
+        let minVal = runner.value ;
+        while (runner.left!==null){
+            if (minVal < runner.left.value){
+                minVal=runner.left.value
+            } 
+            runner = runner.left 
+        }
+        return minVal
+       
+    }
+    max (){
+        if (this.isEmpty())
+        {
+            return false
+        }
+        let runner = this.root;
+        let maxVal = runner.value
+        while(runner.right!==null){
+            if (maxVal>runner.right.value){
+                maxVal=runner.right.value
+            }
+        }
+        return maxVal
+
+    }
+
+}
+
+var node1 = new Node (8);
+var node2 = new Node (45);
+var node3 = new Node (6);
+node1.right = node2 ;
+node1.left = node3 ; 
+var myBst = new BST ();
+console.log(myBst.isEmpty());
+myBst.root= node1;
+console.log(myBst.isEmpty());
+console.log(myBst.min());
